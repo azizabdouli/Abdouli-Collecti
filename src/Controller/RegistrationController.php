da308bde2223318c46980494bdd167d184388307
azizabdouli <azizabdouli601@gmail.com>
2023-04-17 01:39:34 +0100

    17/04/2023

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -52,15 +52,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation(
-                'app_verify_email',
-                $user,
-                (new TemplatedEmail())
-                    ->from(new Address('azizabdouli601@gmail.com', 'Azizabdouli'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+           
             // do anything else you need here, like send an email
 
             return $userAuthenticator->authenticateUser(
